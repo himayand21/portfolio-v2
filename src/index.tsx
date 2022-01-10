fb2e6cbc52c1cc9eebd62cded9779e7bc8a623b0
Himayan Debnath <himayand21@gmail.com>
2022-01-10 11:42:24 +0530

    perf(pwa): add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import App from './App';
 
 ReactDOM.render(
@@ -12,3 +14,8 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
